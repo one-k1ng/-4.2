73604aefb480d32cd05628c135f2fbc79a95c30e
Администратор <@DESKTOP-PHORFLN>
2020-09-22 22:54:22 +0300

    123

---
+++ Task2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Практика4._2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("'From Stack: {0}", obj);
+            }
+        }
+    }
+}
