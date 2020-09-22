3a702560ae7e65ec63b17824ee1cf516f916afa9
Администратор <@DESKTOP-PHORFLN>
2020-09-22 17:32:09 +0300

    Добавьте файлы проекта.

---
+++ Task1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Практика4._2
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First"); 
+            queue.Enqueue("Second"); 
+            queue.Enqueue("Third"); 
+            queue.Enqueue("Fourth");
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+                Console.WriteLine("From Queue:{0}", obj);
+            }
+        }
+    }
+}
